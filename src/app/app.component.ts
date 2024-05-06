190b8953a877656923259ef19204e463a2c094d7
hoangnm.ndm <hoangnm.ndm@gmail.com>
2024-05-06 13:33:24 +0700

    Lesson 1: install bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,21 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'ahihi',
+  selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
-  template: '<h1>Hello ca lop!</h1>',
-  styles: 'h1 { color: red}',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'WD18332';
+  userInfor = {
+    name: 'Nguyễn Minh Hoàng',
+    age: 18,
+    address: 'Lục Nam - Bắc Giang',
+    email: 'hoangnm62@fpt.edu.vn',
+  };
+  hello() {
+    console.log('Xin chao moi nguoi!');
+  }
 }
