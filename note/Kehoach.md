a1167a8f1bd40d29ec0df02e9c9ffcc1407ff292
hoangnm.ndm <hoangnm.ndm@gmail.com>
2024-05-08 14:41:11 +0700

    Lesson 2: Sumary

--- note/Kehoach.md
+++ note/Kehoach.md
@@ -6,3 +6,11 @@
 - Cài đặt bootstrap
 
 # Lesson 2:
+
+- Cài đặt tailwindcss
+- Cài đặt các extension cần thiết
+- Interpolation
+- Data binding (property binding, event binding, two-way binding)
+- Directive: *ngIf, *ngFor.
+
+# Lesson 3:
--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,10 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Product } from '../../interfaces/Product';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss',
 })
-export class HomePageComponent {}
+export class HomePageComponent {
+  products: Product[] = [
+    { id: 1, name: 'xeng?', price: 1000 },
+    { id: 1, name: 'cuoc', price: 2000 },
+  ];
+}
