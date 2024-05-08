dea793d6d40fb30ebe4bca4bad332b5287beed97
hoangnm.ndm <hoangnm.ndm@gmail.com>
2024-05-08 13:11:38 +0700

    Lesson 2: data binding

---
+++ note/Kehoach.md
@@ -0,0 +1,8 @@
+# Lesson 1:
+
+- Cài đặt Angular CLI
+- Tạo một ứng dụng Angular mới
+- Tạo component đầu tiên
+- Cài đặt bootstrap
+
+# Lesson 2:
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,37 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ProductListComponent } from './product-list/product-list.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { NotFoundPageComponent } from './pages/not-found-page/not-found-page.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ProductListComponent],
+  imports: [
+    RouterOutlet,
+    ProductListComponent,
+    HomePageComponent,
+    NotFoundPageComponent,
+    FormsModule,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'WD18332';
-  userInfor = {
+  user = {
     name: 'Nguyễn Minh Hoàng',
     age: 18,
     address: 'Lục Nam - Bắc Giang',
     email: 'hoangnm62@fpt.edu.vn',
   };
+
   hello() {
     console.log('Xin chao moi nguoi!');
+  }
+
+  showInfor() {
+    console.log('Hien thi thong tin ca nhan!');
   }
 }
