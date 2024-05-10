4d9ed2f018131551af4953951558c82e8522ccd6
hoangnm.ndm <hoangnm.ndm@gmail.com>
2024-05-10 12:24:51 +0700

    Lesson 2: truyen nhan du lieu qua cac component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import { ProductListComponent } from './product-list/product-list.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { NotFoundPageComponent } from './pages/not-found-page/not-found-page.component';
 import { FormsModule } from '@angular/forms';
+import { Product } from './interfaces/Product';
 
 @Component({
   selector: 'app-root',
@@ -19,19 +20,5 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'WD18332';
-  user = {
-    name: 'Nguyễn Minh Hoàng',
-    age: 18,
-    address: 'Lục Nam - Bắc Giang',
-    email: 'hoangnm62@fpt.edu.vn',
-  };
-
-  hello() {
-    console.log('Xin chao moi nguoi!');
-  }
-
-  showInfor() {
-    console.log('Hien thi thong tin ca nhan!');
-  }
+  products: string = 'iphone 11';
 }
--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Product } from '../interfaces/Product';
 
 @Component({
   selector: 'app-product-list',
   standalone: true,
   imports: [],
   templateUrl: './product-list.component.html',
-  styleUrl: './product-list.component.scss'
+  styleUrl: './product-list.component.scss',
 })
 export class ProductListComponent {
-
+  constructor() {}
+  @Input() data: string | undefined;
+  @Input() style: string | undefined;
 }
