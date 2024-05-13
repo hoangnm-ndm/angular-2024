57b2ff7e839e25d02db543f61c351ab26ff0fab4
hoangnm.ndm <hoangnm.ndm@gmail.com>
2024-05-13 14:25:39 +0700

    Login Page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { AboutPageComponent } from './pages/about-page/about-page.component';
 import { NotFoundPageComponent } from './pages/not-found-page/not-found-page.component';
+import { LoginComponent } from './pages/login/login.component';
 
 export const routes: Routes = [
   { path: '', component: HomePageComponent },
@@ -12,6 +13,10 @@ export const routes: Routes = [
   {
     path: 'about',
     component: AboutPageComponent,
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
   },
   {
     path: '**',
