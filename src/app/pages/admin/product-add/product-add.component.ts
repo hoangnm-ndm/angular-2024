3d3f70d59d55fc1dd02a4e6b40852c9ccb3d25fc
hoangnm.ndm <hoangnm.ndm@gmail.com>
2024-05-24 12:36:38 +0700

    Lesson 9: Reactive form

--- src/app/pages/admin/product-add/product-add.component.ts
+++ src/app/pages/admin/product-add/product-add.component.ts
@@ -1,26 +1,38 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from '../../../interfaces/Product';
-import { FormsModule } from '@angular/forms';
+import {
+  Form,
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+} from '@angular/forms';
 import { ProductService } from '../../../product.service';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-product-add',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, ReactiveFormsModule],
   templateUrl: './product-add.component.html',
   styleUrl: './product-add.component.scss',
 })
 export class ProductAddComponent implements OnInit {
   product: Product = {} as Product;
-
-  constructor(private productService: ProductService, private router: Router) {}
+  productForm: FormGroup = {} as FormGroup;
+  constructor(
+    private productService: ProductService,
+    private router: Router,
+    private fb: FormBuilder
+  ) {
+    this.productForm = this.fb.group({
+      title: [''],
+      price: [0],
+      description: [''],
+    });
+  }
   ngOnInit(): void {}
   handleSubmit() {
-    this.productService.createProduct(this.product).subscribe((product) => {
-      console.log('Success!', product);
-      // chuyen huong trang
-      this.router.navigate(['/admin']);
-    });
+    console.log(this.productForm.value);
   }
 }
