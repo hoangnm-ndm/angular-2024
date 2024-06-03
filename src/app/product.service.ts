eb13260c2aac8cd552fb9f3d4c3062fd6b765430
hoangnm <hoangnm.ndm@gmail.com>
2024-06-03 13:41:30 +0700

    add service api

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -6,7 +6,7 @@ import { Product } from './interfaces/Product';
   providedIn: 'root',
 })
 export class ProductService {
-  private baseURL = 'http://localhost:3000/products';
+  private baseURL = 'https://hoangnm-json.onrender.com';
   constructor(private http: HttpClient) {}
   getProducts() {
     return this.http.get<Product[]>(this.baseURL);
